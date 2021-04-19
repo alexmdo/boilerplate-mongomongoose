61f99162eacb753cf1fc60c238a827a5060e214a
Alexandre Oliveira <>
2021-04-19 02:42:03 +0000

    Chain Search Query Helpers to Narrow Search Results

--- myApp.js
+++ myApp.js
@@ -101,7 +101,19 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person
+    .find({ favoriteFoods: foodToSearch })
+    .sort("name")
+    .limit(2)
+    .select("-age")
+    .exec((err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    });
+
+
+
+
 };
 
 /** **Well Done !!**
